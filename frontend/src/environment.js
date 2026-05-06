a35ecffe1543935ee436ba8ad4b1c71e4bf36068
Saketraj234 <saketbarnwal468@gmail.com>
2026-05-06 11:50:18 +0530

    add link frontend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 const IS_PROD = true;
 const server = IS_PROD ?
     "https://teem-meet-backend.onrender.com" :
-    "http://localhost:8000";
+    "https://team-meet-liart.vercel.app/";
 
 export default server;
